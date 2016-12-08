a37cc0ea2a5798462378a028020dcc98bd7cb8b9
Philippe Bernard <philippe.bernard@gmail.com>
2016-12-08 10:50:01 +0100

    Update RFG latest version

--- test/check_for_updates.js
+++ test/check_for_updates.js
@@ -14,7 +14,10 @@ describe('checkForUpdates', function() {
   });
 
   it('should return no error when no new version is available', function(done) {
-    realFavicon.checkForUpdates(0.11, function(err) {
+    // The hardcoded version should match the current version returned
+    // by RFG. So if you see this test failing, first check that the version
+    // below is the one at https://realfavicongenerator.net/change_log
+    realFavicon.checkForUpdates(0.13, function(err) {
       assert.equal(err, undefined);
       done();
     });
